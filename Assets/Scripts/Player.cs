9bd3addc9dd79377ed7c1910892ee81beebdc700
crannnabethadh <crannnabethadh@outlook.com>
2020-02-07 18:00:29 +0100

    Start

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+[Serializable]
+public class Player
+{
+    public string Id;
+    public string Email;
+    public string FirstName;
+    public string LastName;
+    public string NickName;
+    public string City;
+    public string DateOfBirth;
+    public string DateJoined;
+    public string BlobUri;
+}
